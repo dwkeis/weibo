a3583083b28b5e7bb1f0e554693f88199eeb91f2
Ma Keng Chon <dwkeis@gmail.com>
2020-06-16 18:02:39 +0800

    slice header and footer into views

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target=_blank>
+    Mean to Practise, Improve everyday.
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >About</a>
+  </div>
+</footer>
