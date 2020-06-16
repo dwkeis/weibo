88ae52ee81a39615011809c7bba93f0c3128cbf2
Ma Keng Chon <dwkeis@gmail.com>
2020-06-16 17:03:24 +0800

    static file browser cache problem

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,14 +2,14 @@
 <html>
   <head>
     <title>@yield('title','Weibo App') - Newbie of Laravel</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     	<div class="container">
     		<a class="navbar-brand" href="/">WeiBo App</a>
     		<ul class="navbar-nav justify-content-end">
-    			<li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
+    			<li class="nav-item"><a class="nav-link" href="/help">Help`</a></li>
     			<li class="nav-item"><a class="nav-link" href="#">Log in</a></li>
     		</ul>
     	</div>
