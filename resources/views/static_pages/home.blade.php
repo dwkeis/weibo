0d634fa2c4d7a08c14b11c85de281a760a76eb05
Ma Keng Chon <dwkeis@gmail.com>
2020-06-16 11:55:07 +0800

    初始化

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-	<h1>Home Page</h1>
+	<div class="jumbotron">
+		<h1>Home Page</h1>
+		<p class="lead">
+			Now you watching is <a href="https://learnku.com/courses/laravel-essential-training">Laravel intro</a>'s sample home page
+		</p>
+		<p>
+			Everything, strat from here.
+		</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="#" role="button">Register now</a>
+		</p>
+	</div>
 @stop
